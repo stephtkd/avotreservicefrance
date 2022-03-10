bc4374682424dfeee414ef541bd6e26b320679ac
Stéphane Burlet <stephtkd@yahoo.fr>
2022-03-10 13:48:38 +0100

    nettoyage

--- res.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// phpinfo();
-ini_set('display_errors', 1);
-            error_reporting(E_ALL);
-            ini_set("error_log","error_php.txt");
-
-            echo print_r($_POST,true);
-?>
